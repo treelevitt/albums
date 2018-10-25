32bb16ab22401eaae7cbcc0dc502826b237454f3
Teresa Leong <TeresaLeong@Teresas-MacBook-Pro.local>
2018-10-25 16:16:21 -0400

    Fourth committ

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
+import Card from './Card';
 
-const AlbumDetail = (props) => (
-        <View>
-            <Text>{props.album.title}</Text>
-        </View>
-    );
+const AlbumDetail = (props) => {
+  return (
+    <Card>
+      <Text>{props.album.title}</Text>
+    </Card>
+  );
+};
 
 export default AlbumDetail;
